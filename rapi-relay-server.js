d1defd705845cc3e6120e9cfd95f692cc86f86fc
dprasen <dprasen@gmail.com>
2017-09-11 20:35:44 +0530

    added disconnect event
    added disconnect event

--- rapi-relay-server.js
+++ rapi-relay-server.js
@@ -12,4 +12,8 @@ io.on('connection', function (socket) {
   socket.on('my other event', function (data) {
     console.log(data);
   });
+  socket.on('disconnect', function () {
+    io.emit('client disconnected');
+  });
+
 });
